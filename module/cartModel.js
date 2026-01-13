c25e9726de2dccf4c37ba84f0a059606284782d8
kiranaswamy <kiranswamygowda72@gmail.com>
2026-01-13 13:53:11 +0530

    multipal carts

--- module/cartModel.js
+++ module/cartModel.js
@@ -1,3 +1,53 @@
+// const getdb = require('../util/dbConnection').getdb;
+// const mongodb = require('mongodb');
+
+// class Cart {
+//   static addToCart(userId, productId) {
+//     const db = getdb();
+
+//     return db.collection('carts').findOne({ userId })
+//       .then(cart => {
+//         if (!cart) {
+//           // create new cart
+//           return db.collection('carts').insertOne({
+//             userId,
+//             items: [{ productId, quantity: 1 }]
+//           });
+//         }
+
+//         const existingItemIndex = cart.items.findIndex(
+//           item => item.productId === productId
+//         );
+
+//         if (existingItemIndex >= 0) {
+//           cart.items[existingItemIndex].quantity += 1;
+//         } else {
+//           cart.items.push({ productId, quantity: 1 });
+//         }
+
+//         return db.collection('carts').updateOne(
+//           { userId: uId },
+//           { $set: { items: cart.items } }
+//         );
+//       });
+//   }
+
+//   static getCart(userId) {
+//     const db = getdb();
+//     return db.collection('carts').findOne({ userId });
+//   }
+
+//   static removeFromCart(userId, productId) {
+//     const db = getdb();
+//     return db.collection('carts').updateOne(
+//       { userId },
+//       { $pull: { items: { productId } } }
+//     );
+//   }
+// }
+
+// module.exports = Cart;
+
 const getdb = require('../util/dbConnection').getdb;
 const mongodb = require('mongodb');
 
@@ -5,43 +55,50 @@ class Cart {
   static addToCart(userId, productId) {
     const db = getdb();
 
-    return db.collection('carts').findOne({ userId })
+    const uId = new mongodb.ObjectId(userId);
+    const pId = new mongodb.ObjectId(productId);
+
+    // ✅ FIXED
+    return db.collection('carts').findOne({ userId: uId })
       .then(cart => {
         if (!cart) {
-          // create new cart
           return db.collection('carts').insertOne({
-            userId,
-            items: [{ productId, quantity: 1 }]
+            userId: uId,
+            items: [{ productId: pId, quantity: 1 }]
           });
         }
 
         const existingItemIndex = cart.items.findIndex(
-          item => item.productId === productId
+          item => item.productId.toString() === pId.toString()
         );
 
         if (existingItemIndex >= 0) {
           cart.items[existingItemIndex].quantity += 1;
         } else {
-          cart.items.push({ productId, quantity: 1 });
+          cart.items.push({ productId: pId, quantity: 1 });
         }
 
+        // ✅ FIXED
         return db.collection('carts').updateOne(
-          { userId },
+          { userId: uId },
           { $set: { items: cart.items } }
         );
       });
   }
 
+  // ✅ FIXED
   static getCart(userId) {
     const db = getdb();
-    return db.collection('carts').findOne({ userId });
+    return db.collection('carts').findOne({
+      userId: new mongodb.ObjectId(userId)
+    });
   }
 
   static removeFromCart(userId, productId) {
     const db = getdb();
     return db.collection('carts').updateOne(
-      { userId },
-      { $pull: { items: { productId } } }
+      { userId: new mongodb.ObjectId(userId) },
+      { $pull: { items: { productId: new mongodb.ObjectId(productId) } } }
     );
   }
 }
